d7e9e1c1a707e91c12b163d3298e10a8ba135b4e
uhdang <uhdang@gmail.com>
2018-06-22 17:58:21 +0200

    three from set 2

---
+++ Assignments/02_flow_of_control/Set_2/02.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // until 6
+    // s = 1+2+3+ ... +n;
+    // s = n+(n-1)+(n-2)+ ... n-(n-1)
+    // 2s = (n+1) + (n+1) + (n+1) + ...(n+1)
+    // s = n(n+1)/2
+    
+    // sum of first 10 natural number.
+    // n = 10
+    cout << 10 * 11 / 2 << "\n";
+}
---
+++ Assignments/02_flow_of_control/Set_2/03.cpp
@@ -0,0 +1,16 @@
+// factorial
+#include <iostream>
+using namespace std;
+
+int fact(int n) {
+    if (n==1)
+        return n*1;
+    return n*fact(n-1);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << fact(n) << "\n";
+    return 0;
+}
